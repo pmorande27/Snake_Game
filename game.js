4a1e3508b414412bfbd6966cd5bae2a6668a6da4
pmorande27 <55888240+pmorande27@users.noreply.github.com>
2020-05-03 12:44:35 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var height = 600;
 var width = 600;
-var scl = 20;
+var scl = 5;
 var fruit;
 var s;
 function setup(){
@@ -41,4 +41,56 @@ function pickLocation(){
 	food = createVector(floor(random(cols)),floor(random(rows)));
 	food.mult(scl);
 
+}
+function Snake(){
+	this.x = 0;
+	this.y=0;
+	this.vx= 1;
+	this.vy = 0;
+	this.total = 0;
+	this.tailPos = [];
+	this.update = function(){
+		if ( this.total === this.tailPos.length )
+		for(var i = 0; i <this.total-1; i++){
+			this.tailPos[i] = this.tailPos[i+1];
+		}
+		this.tailPos[this.total-1] = createVector(this.x,this.y);
+
+		this.x += this.vx*scl;
+		this.y += this.vy*scl;
+		this.x = constrain(this.x,0,width-scl);
+		this.y = constrain(this.y, 0 , height-scl);
+	}
+	this.die = function(){
+		for(var i= 0; i<this.tailPos.length;i++){
+			var pos = this.tailPos[i];
+			var d = dist(this.x,this.y,pos.x,pos.y);
+			if (d<1){
+				this.total = 0;
+				this.tailPos = [];
+			}
+		}
+
+	}
+	this.eat = function(pos){
+		var distance = dist(this.x,this.y,pos.x,pos.y);
+		if (distance<1){
+            this.total++;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	this.dir = function(x,y){
+		this.vx = x;
+		this.vy= y;
+	}
+	this.show = function(){
+		fill(255);
+		rect(this.x, this.y, scl, scl);
+		for(var i = 0; i<this.tailPos.length; i++){
+			rect(this.tailPos[i].x,this.tailPos[i].y,scl,scl);
+		}
+	}
 }
